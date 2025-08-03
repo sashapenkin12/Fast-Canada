93cb5f3d9e5a8f62f5b4d8b87fb1b6d3a997eb73
aleksanr_penkin <sashapenkin12@mail.ru>
2025-08-03 15:08:20 +0600

    docs(cart): write docstring for cart app

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,16 +1,45 @@
+"""
+Serializers for cart app.
+"""
+
 from rest_framework import serializers
 
 
 class CartProductSerializer(serializers.Serializer):
+    """
+    Serializer for representing products in DB.
+
+    Attributes:
+        title: Title of the product.
+        price: Product price.
+    """
     title = serializers.CharField()
     price = serializers.DecimalField(max_digits=10, decimal_places=2)
 
 
 class CartItemSerializer(serializers.Serializer):
+    """
+    Serializer for representing cart items.
+
+    Attributes:
+        id: Cart item ID.
+        product: Cart item's product.
+        count: Products count.
+        total_price: Total price of specified amount of products.
+    """
     id = serializers.IntegerField()
     product = CartProductSerializer()
     count = serializers.IntegerField()
     total_price = serializers.SerializerMethodField()
 
-    def get_total_price(self, obj):
+    def get_total_price(self, obj) -> int:
+        """
+        Get total price of specified amount of products.
+
+        Attributes:
+            obj: Serializer object.
+
+        Returns:
+            int: Total price.
+        """
         return obj['product']['price'] * obj['count']
--- cart/services.py
+++ cart/services.py
@@ -1,4 +1,17 @@
+"""
+Service functions for working with database.
+"""
+
 from household_chemicals.models import ChemicalProduct
 
+
 def get_product_by_id(product_id: int) -> ChemicalProduct:
+    """
+    Get product by its ID.
+
+    Args:
+        product_id: ID of the product.
+    Returns:
+        ChemicalProduct: ChemicalProduct object with specified ID.
+    """
     return ChemicalProduct.objects.get(id=product_id)
