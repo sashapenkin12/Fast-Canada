a43cfa5d9cea815de1b65a065bc863abb697f872
aleksanr_penkin <sashapenkin12@mail.ru>
2025-08-04 11:15:03 +0600

    fix(orders): fix test data & OrderSerializer
    # Conflicts:
    #	orders/tests.py

--- orders/serializers.py
+++ orders/serializers.py
@@ -8,15 +8,16 @@ class OrderSerializer(serializers.Serializer):
     full_name = serializers.CharField(max_length=255)
     address = serializers.CharField(max_length=255)
     phone_number = serializers.CharField()
-    email = serializers.EmailField()
+    email = serializers.EmailField(required=False)
     items = CartItemSerializer(many=True, allow_empty=False)
+    comment = serializers.CharField(style={'base_template': 'textarea.html'}, allow_blank=True, required=False)
 
 
     def validate_phone_number(self, value):
         try:
             parsed = phonenumbers.parse(value, None)
             if not phonenumbers.is_valid_number(parsed):
-                raise serializers.ValidationError("Некорректный номер телефона")
+                raise serializers.ValidationError("Invalid phone number.")
         except phonenumbers.NumberParseException:
-            raise serializers.ValidationError("Неверный формат номера")
+            raise serializers.ValidationError("Wrong phone number format.")
         return value
