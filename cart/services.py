b19a6b4a50022c43d321f7d3cac3cc290af7371b
aleksanr_penkin <sashapenkin12@mail.ru>
2025-08-05 14:43:32 +0600

    refactor(cart): write docstring for services.py

--- cart/services.py
+++ cart/services.py
@@ -1,3 +1,7 @@
+"""
+Services for cart app.
+"""
+
 from abc import ABC, abstractmethod
 from typing import Callable, Optional
 
@@ -7,29 +11,73 @@ from .crud import get_product_by_id
 from .serializers import CartProductSerializer, CartItemSerializer
 
 class CartStorage(ABC):
+    """
+    Cart storage abstraction.
+
+    Methods:
+        load: Load storage.
+        save: Save changes in storage.
+    """
     @abstractmethod
     def load(self) -> list[dict]:
+        """
+        Load storage serialized object.
+
+        Returns:
+            list[dict]: Serialized python object for cart serving.
+        """
         pass
 
     @abstractmethod
     def save(self, cart: list[dict]) -> None:
+        """
+        Save changes in serialized object to storage.
+
+        Args:
+            cart: Serialized python object with changes.
+        """
         pass
 
 
 class SessionCartStorage(CartStorage):
+    """
+    Cart storage realization with Django session.
+    """
     def __init__(self, session: dict, session_key: str) -> None:
+        """
+        Init a session storage.
+
+        Args:
+            session: Current request session.
+            session_key: Cart session key.
+        """
         self.session = session
         self.session_key = session_key
 
     def load(self) -> list[dict]:
+        """
+        Load current session cart.
+
+        Returns:
+            list[dict]: Current cart.
+        """
         return self.session.get(self.session_key, [])
 
     def save(self, cart: list[dict]) -> None:
+        """
+        Save changed cart to session.
+
+        Args:
+            cart: Modified cart.
+        """
         self.session[self.session_key] = cart
         self.session.modified = True
 
 
 class CartManager:
+    """
+    Manager for working with cart.
+    """
     def __init__(self, storage: CartStorage):
         self.storage = storage
 
@@ -44,6 +92,15 @@ class CartManager:
         return False
 
     def add_to_cart(self, item_data: dict) -> None:
+        """
+        Add item to the cart.
+
+        Args:
+            item_data: Item data.
+
+        Raises:
+            ValidationError: If data isn't matching structure.
+        """
         item_data.setdefault('count', 1)
         product_id = item_data.get('product')
         if not product_id:
@@ -64,12 +121,15 @@ class CartManager:
         if not cart_item.is_valid():
             raise ValidationError(detail=f'Invalid request data: {cart_item.errors}')
 
-        add_cart_item(
-            cart_item=cart_item.data,
-            cart=self.cart,
-        )
+        self.cart.append(cart_item.data)
 
     def remove_from_cart(self, item_id: int) -> None:
+        """
+        Remove item from cart by ID.
+
+        Args:
+            item_id: Item ID.
+        """
         for cart_item in self.cart:
             if cart_item.get('id') == item_id:
                 self.cart.remove(cart_item)
@@ -85,24 +145,41 @@ class CartManager:
         )
 
 
-def increment_item_count(cart: list, index: int):
+def increment_item_count(cart: list, index: int) -> None:
+    """
+    Increment item count in the cart.
+
+    Args:
+        cart: Current cart.
+        index: Item cart index.
+    """
     cart[index]['count'] += 1
 
 
 def check_duplicate(cart: list, product_title: str) -> Optional[int]:
+    """
+    Check cart on duplicates. If found, returns index.
+
+    Args:
+        cart: Current cart.
+        product_title: Product title.
+
+    Returns:
+        Optional[int]: If found, returns index, else None
+    """
     for index, cart_item in enumerate(cart):
         if product_title == cart_item['product']['title']:
             return index
 
 
-def add_cart_item(
-        cart_item: dict,
-        cart: list[dict],
-):
-    cart.append(cart_item)
+def remove_if_zero(cart: list, cart_item: dict) -> None:
+    """
+    Specifies actions for item count equals 0 scenario.
 
-
-def remove_if_zero(cart: list, cart_item: dict):
+    Args:
+        cart: Current cart.
+        cart_item: Cart item, which count is equals 0.
+    """
     cart.remove(cart_item)
 
 
@@ -112,6 +189,15 @@ def apply_item_delta(
         delta: int,
         on_zero: Callable[[list, dict], None] = None
 ):
+    """
+    Specifies actions for item count equals 0 scenario.
+
+    Args:
+        cart: Current cart.
+        item_id: Item for changing.
+        delta: Difference between new value and old value.
+        on_zero: Action for objects, which count equals zero.
+    """
     for cart_item in cart:
         if cart_item.get('id') == item_id:
             cart_item['count'] += delta
